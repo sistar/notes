aee5d94ff1d0cf507623e0a513d518bca65085dd
Ralf Sigmund <ralf.sigmund@gmail.com>
2013-06-05 14:59:47 +0200

    merge schnellste schwimmzeit zu gesamtzeit

--- clojure/midje-demo/src/midje_demo/parse_tria_results.clj
+++ clojure/midje-demo/src/midje_demo/parse_tria_results.clj
@@ -6,25 +6,12 @@
   (:require [clojure.java.io :as io])
   (:require [clj-http.client :as client]))
 
+(def age-groups {2013 {"c" ["2005" "2004"] "b" ["2003" "2002"] "a" ["2001" "2000"] "sb" ["1999" "1998"]}})
 (defn map-tag [tag xs]
   (map (fn [x] [tag x]) xs))
 
 (defn call-verein-service [startnummer]
-  (:verein (:body(client/get (str "http://localhost:8000/collection" "/" startnummer) {:accept :json :as :json}))))
-
-(defn out [sq]
-  (def counter (atom 0))
-
-  (html [:table [:tr (map-tag :th ["Platzierung" "Name" "Verein" "Jahrgang" "Gesamtzeit"])]
-         (map
-           (fn [m]
-              [:tr [:td (swap! counter inc)]
-               [:td (m :name )]
-               [:td (call-verein-service (m :startnummer ))]
-               [:td (m :jg )]
-               [:td (m :ziel-zeit )]]
-              )
-           sq)]))
+  (:verein (:body (client/get (str "http://localhost:8000/collection" "/" startnummer) {:accept :json :as :json}))))
 
 (defn grabData [file]
   (str/split-lines (slurp file)))
@@ -46,14 +33,10 @@
 
 (defn agegroup-pred [yrs m] (contains? yrs (m :jg )))
 
-(defn to-result-table
-  ([lsq] (out (map-flds (rest lsq))))
-  ([lsq pred] (out (filter pred (map-flds (rest lsq))))))
 
 (defn in?
   "true if seq contains elm"
   [seq elm]
-  (prn seq elm)
   (some #(= elm %) seq))
 
 (defn second-line-last-col [seq]
@@ -69,14 +52,98 @@
                     "b" ["m" "w"]
                     "a" ["x"]
                     "j" ["x"]})
+(defn result-files-gender
+  ([m] (result-files-gender (m :age ) (m :gender )))
+  ([age-group gender]
 
-(defn swim-times [age-group]
-  (map
-    #(second-line-last-col (csv-seq-path (result-file age-group %)))
-    (age-to-gender age-group)))
+    (rest (csv-seq-path (result-file age-group gender))
+      ))
+  )
+(defn swim-times []
+  (apply concat (map result-files-gender (for [age (keys age-to-gender) gender (age-to-gender age) :let [t {:age age :gender gender}]] t)))
+  )
+(defn startnummer-to-swimtimes []
+  (apply merge (filter #(not (empty? (first (vals %)))) (map #(hash-map (second %) (last %)) (swim-times))))
+  )
+(defn rad-laufen-zeit [startnummer]
+
+  )
+(defn out [sq]
+  (def counter (atom 0))
+
+  (html [:table [:tr (map-tag :th ["Platzierung" "Name" "Verein" "Jahrgang" "Schwimmen" "Rad/Laufen" "Gesamtzeit"])]
+         (map
+           (fn [m]
+             [:tr [:td (swap! counter inc)]
+              [:td (m :name )]
+              [:td (call-verein-service (m :startnummer ))]
+              [:td (m :jg )]
+              [:td ((startnummer-to-swimtimes) (m :startnummer ))]
+              [:td (rad-laufen-zeit (m :startnummer ))]
+              [:td (m :ziel-zeit )]]
+             )
+           sq)]))
+
+(defn to-result-table
+  ([lsq time-modifier] (out (map time-modifier (map-flds (rest lsq)))))
+  ([lsq pred time-modifier] (out (map time-modifier(filter pred (map-flds (rest lsq)))))))
+
+(defn best-swim-times
+  ([age-group gender]
+    (second-line-last-col (csv-seq-path (result-file age-group gender))))
+  ([age-group]
+    (map
+      #(best-swim-times age-group %)
+    (age-to-gender age-group))))
 
 (defn first-str [times]
   (first (sort times)))
 
-(defn min-swim-time [age-group]
-  (first-str (swim-times age-group)))
+(defn min-swim-time
+  ([age-group gender]
+    (if (= (count (age-to-gender age-group)) 1)
+      (first-str (best-swim-times age-group))
+      (best-swim-times age-group gender)
+      ))
+  ([age-group]
+    (first-str (best-swim-times age-group))))
+(defn parse-int [s]
+  (Integer. (re-find #"\d+" s)))
+(defn add-timestr [a b]
+  (if (or (empty? a) (empty? b))
+    nil
+    (let [[h m s] (map + (map parse-int (.split a ":")) (map parse-int (.split b ":")))]
+      (let [carry-s-to-m (quot s 60)
+            correct-s (mod s 60)
+            carry-m-to-h (quot (+ m carry-s-to-m) 60)
+            correct-m (mod (+ m carry-s-to-m) 60)
+            correct-h (+ h carry-m-to-h)
+            ]
+        (format "%d:%02d:%02d" correct-h correct-m correct-s)))))
+
+(defn add-fastest-swimtime-in-group [fastest-time-to-add-timestr m]
+  (assoc m :ziel-zeit (add-timestr fastest-time-to-add-timestr (m :ziel-zeit))))
+
+(def infile-keys {"c" "c",
+                  "b" "b",
+                  "a" "a",
+                  "sb" "a"})
+(defn generate-html-tables-for-age-key [year age-key]
+  (prn ">>generating report for" year age-key)
+  (doall (map (fn [gender-key]
+                (let [mst (min-swim-time age-key)
+                      time-modifier (partial add-fastest-swimtime-in-group mst)
+                      infile-key (infile-keys age-key)
+                      infile (format "in-file-%s-%s.csv" gender-key infile-key)
+                      outfile (format "/tmp/%s-%s.html" gender-key age-key)
+                      ]
+                  (spit outfile
+                    (to-result-table
+                      (csv-seq infile)
+                      year
+                      age-key
+                      time-modifier))))
+           ["w" "m"])))
+(defn generate-html-tables [year age-keys]
+  (prn ">>age-keys" age-keys)
+  (doall (map (fn [age-key] (generate-html-tables-for-age-key year age-key)) age-keys)))
--- clojure/midje-demo/test/midje_demo/t_parse_tria_results.clj
+++ clojure/midje-demo/test/midje_demo/t_parse_tria_results.clj
@@ -1,72 +1,96 @@
 (ns midje-demo.t-parse-tria-results
-	(:use midje.sweet)
-	(:use midje-demo.parse-tria-results))
-(facts "about out"
-	(fact "it processes a sequence"
-		(out (sequence[{:platz "1.",:startnummer "2" ,:name "Lindner, Kira",
-           :jg "2004",:nat "DEU",:verein "VAF TriKids" ,:ak "" ,:akp "",
-           :mw-platz "1." ,:ziel-zeit "0:08:10"}])) => "<table><tr><th>Platzierung</th><th>Name</th><th>Verein</th><th>Jahrgang</th><th>Gesamtzeit</th></tr><tr><td>1</td><td>Lindner, Kira</td><td>VAF TriKids</td><td>2004</td><td>0:08:10</td></tr></table>"
-	))
-(facts "about grabData"
-	(fact "it reads a line"
-		(second(let [path "in-file-w-c.csv"]
+  (:use midje.sweet)
+  (:use midje-demo.parse-tria-results))
 
-		(grabData path))) => "1.,2,\"Lindner, Kira\",2004,DEU,VAF TriKids,,,1.,0:08:10" 
-		))
+(facts "about out"
+  (fact "it processes a sequence"
+    (out (sequence [{:platz "1.",:startnummer "2" ,:name "Lindner, Kira",
+                     :jg "2004",:nat "DEU",:verein "VAF TriKids" ,:ak "" ,:akp "",
+                     :mw-platz "1." ,:ziel-zeit "0:08:10"}])) => "<table><tr><th>Platzierung</th><th>Name</th><th>Verein</th><th>Jahrgang</th><th>Gesamtzeit</th></tr><tr><td>1</td><td>Lindner, Kira</td><td>VAF TriKids</td><td>2004</td><td>0:08:10</td></tr></table>"
+    ))
+
+(facts "about `add-timestr`"
+  (fact "adds without carry"
+    (add-timestr "0:02:28" "0:02:28") => "0:04:56")
+  (fact "adds with carry from seconds to minutes"
+    (add-timestr "0:02:28" "0:02:58") => "0:05:26")
+  (fact "adds with carry from seconds to minutes and hours"
+    (add-timestr "1:59:59" "0:59:59") => "2:59:58"))
+
+(facts "about grabData"
+  (fact "it reads a line"
+    (second (let [path "in-file-w-c.csv"]
+
+              (grabData path))) => "1.,2,\"Lindner, Kira\",2004,DEU,VAF TriKids,,,1.,0:08:10"
+    ))
 
 (facts "about csvParseData"
-	(fact "it reads a line"
-		(second (csv-seq "in-file-w-c.csv")) => ["1.","2","Lindner, Kira","2004","DEU","VAF TriKids","","","1.","0:08:10"]
-	))
+  (fact "it reads a line"
+    (second (csv-seq "in-file-w-c.csv")) => ["1.","2","Lindner, Kira","2004","DEU","VAF TriKids","","","1.","0:08:10"]
+    ))
 (facts "about to-result-table"
-	(fact "it works in isolation"
-		(to-result-table (list [] ["1.","2","Lindner, Kira","2004","DEU","VAF TriKids","","","1.","0:08:10"] ))
-		=> "<table><tr><th>Platzierung</th><th>Name</th><th>Verein</th><th>Jahrgang</th><th>Gesamtzeit</th></tr><tr><td>1</td><td>Lindner, Kira</td><td>VAF TriKids</td><td>2004</td><td>0:08:10</td></tr></table>"
-		 )
+  (fact "it works in isolation"
+    (to-result-table (list [] ["1.","2","Lindner, Kira","2004","DEU","VAF TriKids","","","1.","0:08:10"]) (partial add-fastest-swimtime-in-group (min-swim-time "c")))
+    => "<table><tr><th>Platzierung</th><th>Name</th><th>Verein</th><th>Jahrgang</th><th>Gesamtzeit</th></tr><tr><td>1</td><td>Lindner, Kira</td><td>VAF TriKids</td><td>2004</td><td>0:08:10</td></tr></table>"
+    )
 
-	(fact "it generates a table"
-		(spit "/tmp/w-c.html" (to-result-table (csv-seq "in-file-w-c.csv"))) => nil
-		(spit "/tmp/m-c.html" (to-result-table (csv-seq "in-file-m-c.csv"))) => nil
-		(spit "/tmp/w-b.html" (to-result-table (csv-seq "in-file-w-b.csv"))) => nil
-		(spit "/tmp/m-b.html" (to-result-table (csv-seq "in-file-m-b.csv"))) => nil
-	)
-	(fact "it splits schüler a jugend b"
+  (fact "it generates a table"
+    (let [mst (min-swim-time "c")
+          time-modifier (partial add-fastest-swimtime-in-group mst)]
+      (spit "/tmp/w-c.html" (to-result-table (csv-seq "in-file-w-c.csv") time-modifier)) => nil
+      (spit "/tmp/m-c.html" (to-result-table (csv-seq "in-file-m-c.csv") time-modifier)) => nil)
+    (let [mst (min-swim-time "b")
+          time-modifier (partial add-fastest-swimtime-in-group mst)]
+      (spit "/tmp/w-b.html" (to-result-table (csv-seq "in-file-w-b.csv") time-modifier)) => nil
+      (spit "/tmp/m-b.html" (to-result-table (csv-seq "in-file-m-b.csv") time-modifier)) => nil)
+    )
+  (fact "it splits schüler a jugend b"
     (let [s-a-pred (partial agegroup-pred ["2000" "2001"])
-          j-b-pred (partial agegroup-pred ["1998" "1999"])]
-		(spit "/tmp/w-a.html" (to-result-table (csv-seq "in-file-w-a.csv") s-a-pred)) => nil
-		(spit "/tmp/m-a.html" (to-result-table (csv-seq "in-file-m-a.csv") s-a-pred)) => nil
-		(spit "/tmp/w-jb.html" (to-result-table (csv-seq "in-file-w-a.csv") j-b-pred)) => nil
-		(spit "/tmp/m-jb.html" (to-result-table (csv-seq "in-file-m-a.csv") j-b-pred)) => nil
+          j-b-pred (partial agegroup-pred ["1998" "1999"])
+          mst-a (min-swim-time "a")
+          mst-j (min-swim-time "j")
+          time-modifier-a (partial add-fastest-swimtime-in-group mst-a)
+          time-modifier-j (partial add-fastest-swimtime-in-group mst-j)
+          ]
+      (spit "/tmp/w-a.html" (to-result-table (csv-seq "in-file-w-a.csv") s-a-pred time-modifier-a)) => nil
+      (spit "/tmp/m-a.html" (to-result-table (csv-seq "in-file-m-a.csv") s-a-pred time-modifier-a)) => nil
+      (spit "/tmp/w-jb.html" (to-result-table (csv-seq "in-file-w-a.csv") j-b-pred time-modifier-j)) => nil
+      (spit "/tmp/m-jb.html" (to-result-table (csv-seq "in-file-m-a.csv") j-b-pred time-modifier-j)) => nil
       )))
-	
 
 (facts "about map-flds"
-	(fact "my input vector"
-		(second (csv-seq "in-file-w-c.csv")) => ["1." "2" "Lindner, Kira" "2004" "DEU" "VAF TriKids" "" "" "1." "0:08:10"]
-		)
-	(fact "it maps rows from vector to map with known keys"
-    (first(map-flds(vector(second (csv-seq "in-file-w-c.csv"))))) =>  {:platz "1.",:startnummer "2" ,:name "Lindner, Kira",
-                                             :jg "2004",:nat "DEU",:verein "VAF TriKids" ,:ak "" ,:akp "",
-                                             :mw-platz "1." ,:ziel-zeit "0:08:10"}
-		)
-	(fact "it processes a sequence"
-    (first(map-flds (vector(second (csv-seq "in-file-w-c.csv"))))) => {:platz "1.",:startnummer "2" ,:name "Lindner, Kira",
-                                                                :jg "2004",:nat "DEU",:verein "VAF TriKids" ,:ak "" ,:akp "",
-                                                                :mw-platz "1." ,:ziel-zeit "0:08:10"}))
+  (fact "my input vector"
+    (second (csv-seq "in-file-w-c.csv")) => ["1." "2" "Lindner, Kira" "2004" "DEU" "VAF TriKids" "" "" "1." "0:08:10"]
+    )
+  (fact "it maps rows from vector to map with known keys"
+    (first (map-flds (vector (second (csv-seq "in-file-w-c.csv"))))) => {:platz "1.",:startnummer "2" ,:name "Lindner, Kira",
+                                                                         :jg "2004",:nat "DEU",:verein "VAF TriKids" ,:ak "" ,:akp "",
+                                                                         :mw-platz "1." ,:ziel-zeit "0:08:10"}
+    )
+  (fact "it processes a sequence"
+    (first (map-flds (vector (second (csv-seq "in-file-w-c.csv"))))) => {:platz "1.",:startnummer "2" ,:name "Lindner, Kira",
+                                                                         :jg "2004",:nat "DEU",:verein "VAF TriKids" ,:ak "" ,:akp "",
+                                                                         :mw-platz "1." ,:ziel-zeit "0:08:10"}))
 (facts "about min-swim-time"
   (fact "result-file creates java.io.File according to naming conventions"
-    (str(result-file "c" "m")) => "Ergebnisliste-Kiezkindertriathlon 2013 m c s.csv"
+    (str (result-file "c" "m")) => "Ergebnisliste-Kiezkindertriathlon 2013 m c s.csv"
     )
-	(fact "it is able to read an ugly filename"
-		(first (grabData (clojure.java.io/file "Ergebnisliste-Kiezkindertriathlon 2013 m c s.csv"))) => "Platz,Nr.,Name,JG,Nat,Verein/*Ort,AK,AK-P,M/W-P,Zielzeit"
-		)
-	(fact ""
-		(second-line-last-col 
-			(csv-seq-path (clojure.java.io/file "Ergebnisliste-Kiezkindertriathlon 2013 m c s.csv"))) => "0:02:40"
-		)
-	(fact "it finds the minimum swim time for an age group"
-				(min-swim-time "c") => "0:02:28"
-				(min-swim-time "b") => "0:03:24"
-				(min-swim-time "a") => "0:05:56"
-				(min-swim-time "j") => "0:07:25"
-		))
+  (fact "it is able to read an ugly filename"
+    (first (grabData (clojure.java.io/file "Ergebnisliste-Kiezkindertriathlon 2013 m c s.csv"))) => "Platz,Nr.,Name,JG,Nat,Verein/*Ort,AK,AK-P,M/W-P,Zielzeit"
+    )
+  (fact ""
+    (second-line-last-col
+      (csv-seq-path (clojure.java.io/file "Ergebnisliste-Kiezkindertriathlon 2013 m c s.csv"))) => "0:02:40"
+    )
+  (fact "it finds the minimum swim time for an age group"
+    (min-swim-time "c") => "0:02:28"
+    (min-swim-time "b") => "0:03:24"
+    (min-swim-time "a") => "0:05:56"
+    (min-swim-time "j") => "0:07:25"
+    )
+  (fact "it adds min-swim-time to total-time"
+    (let [mst (min-swim-time "c")
+          time-modifier (partial add-fastest-swimtime-in-group mst)
+          lsq (csv-seq "in-file-w-c.csv")]
+      (:ziel-zeit (first (map time-modifier (map-flds (rest lsq)))))
+      => "0:10:38")))
